a59c1a2e4102ccc7e261c6ee617d8c78d06e9f8f
keerthanaprasanna <keerthanaprasanna292@gmail.com>
2024-09-12 16:04:32 +0530

    Add files via upload

---
+++ string 1.c
@@ -0,0 +1,14 @@
+
+void main()
+{
+  int n,i;
+  char str[50];
+  printf("Enter n");
+  scanf("%d",&n);
+  printf("Enter string");
+  scanf("%s",str);
+  for(i=0;i<=n;i++)
+  {
+} 
+  printf("%s\n",str);
+}
